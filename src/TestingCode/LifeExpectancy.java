b383ef92c27bcb42635095b31fc2208b19e96bb7
Dmitry Dubchak <neighborland@outlook.com>
2019-05-27 07:12:43 +0700

    citymarker mod

--- src/TestingCode/LifeExpectancy.java
+++ src/TestingCode/LifeExpectancy.java
@@ -7,10 +7,12 @@ import java.util.Map;
 
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.data.PointFeature;
+import processing.core.PApplet;
+
 
 public class LifeExpectancy {
 	UnfoldingMap map;
-	Map<String, Float> lifeExpByCountry;
+	private Map<String, Float> lifeExpByCountry;
 
 	public void setup() {
 		lifeExpByCountry = loadLifeExpectancyFromCSV("data.csv");
@@ -21,14 +23,14 @@ public class LifeExpectancy {
 
 	private Map<String, Float> loadLifeExpectancyFromCSV(String fileName) {
 		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
-		String rows[] = loadStrings(fileName);
-		for (String row : rows) {
-			String[] columns = row.split(",");
-			if (1 = 1) {
-				float value = Float.parseFloat(columns[5]);
-				lifeExpMap.put(columns[4], value);
-			}
-		}
+//		String rows[] = loadStrings(fileName);
+//		for (String row : rows) {
+//			String[] columns = row.split(",");
+//			if (1 = 1) {
+//				float value = Float.parseFloat(columns[5]);
+//				lifeExpMap.put(columns[4], value);
+//			}
+//		}
 		return lifeExpMap;
 	}
 }
